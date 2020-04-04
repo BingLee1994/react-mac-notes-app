e867bf48875611677ec949aaff6fba9ef59b1c91
Li, Yabing <791188716@qq.com>
2020-04-04 15:18:35 +0800

    refactor wenpack config to support production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const path = require("path");
 const Env = require('./env');
+const webpack = require('webpack');
 
-module.exports = {
-    mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
+let isPrd = process.env.NODE_ENV === 'production';
+
+const config = {
+    mode: isPrd ? 'production' : 'development',
+    devtool: isPrd ? 'nosources-source-map' : 'cheap-module-eval-source-map',
     entry: './client/index.js',
     output: {
         publicPath: '/',
@@ -16,19 +21,40 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                use:'babel-loader'
+                use: 'babel-loader'
             },
             {
                 test: /\.scss$/,
-                loader:['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
+                loader: (function () {
+                    let loader = [
+                        'css-loader',
+                        'resolve-url-loader',
+                        'sass-loader'
+                    ];
+                    if (!isPrd) {
+                        loader.unshift('style-loader');
+                    } else {
+                        loader.unshift(
+                            {
+                                loader: MiniCssExtractPlugin.loader,
+                                options: {
+                                    esModule: false,
+                                },
+                            })
+                    }
+                    return loader;
+                })()
             },
             {
-                test: /\.(ttf|eot|png|svg|jpg|woff|woff2)$/,
-                use: 'file-loader'
+                test: /\.(ttf|eot|otf|woff|woff2)$/,
+                use: 'file-loader?name=font/[name]-[hash].[ext]'
+            },
+            {
+                test: /\.(png|svg|jpg)$/,
+                use: 'file-loader?name=imgs/[name]-[hash].[ext]'
             }
         ]
     },
-    mode: 'development',
     devServer: {
         historyApiFallback: true,
         port: 8000,
@@ -41,6 +67,34 @@ module.exports = {
             filename: 'index.html',
             template: 'index.html',
             minify: true
+        }),
+        new webpack.DefinePlugin({
+            __ENV__: JSON.stringify(process.env.NODE_ENV),
+            VERSION: JSON.stringify('1.0.0')
         })
     ]
 };
+
+if (isPrd) {
+    config.plugins.push(
+        new MiniCssExtractPlugin({
+            filename: 'style/app[hash].css'
+        })
+    );
+
+    config.optimization = {
+        minimizer: [
+            new UglifyJsPlugin({
+                parallel: 4,
+                sourceMap: false,
+                uglifyOptions: {
+                    output: {
+                        comments: false,
+                    }
+                }
+            })
+        ],
+    }
+}
+
+module.exports = config;
